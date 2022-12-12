5fac92c05817ea3cb12ddddcbe953bec1ab8e3da
alexey kudriashov <gvinpin72@gmail.com>
2022-12-12 19:55:14 +0300

    split styles

--- src/components/CoinItem/index.tsx
+++ src/components/CoinItem/index.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { Text, View, Image } from "react-native";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
+import styles from "./styles";
 
 const CoinItem = () => {
   return (
@@ -36,42 +37,3 @@ const CoinItem = () => {
 };
 
 export default CoinItem;
-
-const styles = StyleSheet.create({
-  currencyContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: "#282828",
-    padding: 15,
-  },
-  image: {
-    width: 45,
-    aspectRatio: 1,
-    marginRight: 10,
-  },
-  title: {
-    color: "#FFFFFF",
-    fontSize: 16,
-    fontWeight: "600",
-    marginBottom: 3,
-  },
-  rank: {
-    color: "#FFFFFF",
-    fontWeight: "700",
-  },
-  rankContainer: {
-    backgroundColor: "#585858",
-    paddingHorizontal: 5,
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 5,
-  },
-  text: {
-    color: "#FFFFFF",
-    fontSize: 14,
-    fontWeight: "500",
-    marginRight: 5,
-  },
-});
