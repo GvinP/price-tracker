1d445b18fe297027af4556adf43834804dda9d6f
alexey kudriashov <gvinpin72@gmail.com>
2022-12-13 07:33:07 +0300

    add data and flatlist

--- src/components/CoinItem/index.tsx
+++ src/components/CoinItem/index.tsx
@@ -2,34 +2,30 @@ import { Text, View, Image } from "react-native";
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import styles from "./styles";
+import { CoinItemType } from "../../../App";
 
-const CoinItem = () => {
+const CoinItem: React.FC<CoinItemType> = ({ image, name, symbol, market_cap_rank, current_price, price_change_percentage_24h, market_cap }) => {
   return (
     <View style={styles.currencyContainer}>
-      <Image
-        source={{
-          uri: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-        }}
-        style={styles.image}
-      />
+      <Image source={{ uri: image }} style={styles.image} />
       <View>
-        <Text style={styles.title}>Bitcoin</Text>
+        <Text style={styles.title}>{name}</Text>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <View style={styles.rankContainer}>
-            <Text style={styles.rank}>1</Text>
+            <Text style={styles.rank}>{market_cap_rank}</Text>
           </View>
-          <Text style={styles.text}>BTC</Text>
+          <Text style={styles.text}>{symbol.toUpperCase()}</Text>
           <AntDesign
-            name="caretdown"
+            name={price_change_percentage_24h<0?"caretdown":"caretup"}
             size={12}
-            color={"#FF0000"}
+            color={price_change_percentage_24h<0?"#FF0000":"#00FF00"}
             style={{ marginRight: 5 }}
           />
-          <Text style={styles.text}>0,63%</Text>
+          <Text style={styles.text}>{Math.abs(price_change_percentage_24h)}</Text>
         </View>
       </View>
       <View style={{ marginLeft: "auto" }}>
-        <Text style={styles.title}>17005.05</Text>
+        <Text style={styles.title}>{current_price}</Text>
         <Text style={styles.text}>MCap 327 B</Text>
       </View>
     </View>
